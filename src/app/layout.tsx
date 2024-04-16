ba280534880a98958d1dbbba4c751727b29931bb
gabriel.silva <gabriel.silva@treeinova.com.br>
2024-04-16 17:53:32 -0300

    feat: Implementa storybook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
 import StyledJsxRegistry from '@/lib/registry'
+import { Providers } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StyledJsxRegistry>{children}</StyledJsxRegistry>
+        <StyledJsxRegistry>
+          <Providers>{children}</Providers>
+        </StyledJsxRegistry>
       </body>
     </html>
   )
